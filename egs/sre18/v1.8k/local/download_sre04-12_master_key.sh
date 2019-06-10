e361b0ce4a123ba43df901634c7fcfbfe9bb14e4
Jesus Villalba <jesus.antonio.villalba@gmail.com>
2019-06-10 12:33:35 -0400

    updated link to master key

--- egs/sre18/v1.8k/local/download_sre04-12_master_key.sh
+++ egs/sre18/v1.8k/local/download_sre04-12_master_key.sh
@@ -6,12 +6,12 @@
 # Downloads master key for sre04-12.
 set -e 
 key_name=master_key_sre04-12
-master_key=$key_name/NIST_SRE_segments_key.csv
+master_key=$key_name/NIST_SRE_segments_key.v2.csv
 
 # shareable link:
-# https://drive.google.com/file/d/1ukJNhj0-7_uWsIFWvtEFQHweq-lFG5a7/view?usp=sharing
+# https://drive.google.com/file/d/1znVYgrEuf9C0B1r7qNARB5E_h1EjKRGX/view?usp=sharing
 
-wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1ukJNhj0-7_uWsIFWvtEFQHweq-lFG5a7" -O $key_name.tar.gz
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1znVYgrEuf9C0B1r7qNARB5E_h1EjKRGX" -O $key_name.tar.gz
 tar xzvf $key_name.tar.gz
 
 if [ ! -f $master_key ];then
