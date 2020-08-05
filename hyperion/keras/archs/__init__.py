49ec52cb4aed7de06a79f8c3ae2d07a1fb31e586
Jesus Villalba <jesus.antonio.villalba@gmail.com>
2020-08-04 21:56:48 -0400

    removed deprecated keras stuff 1

--- hyperion/keras/archs/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-from .tdnn import TDNNV1, ResTDNNV1, TDNNV1WithEmbedInputV1, TDNNV1Transpose, TDNNV1TransposeWithEmbedInputV1, TDNNV2
-from .ffnet import FFNetV1
-from .convnet import ConvNetZhang17V1, TDConv2DNetV1
-from .vgg import VGGNetV1, VGG16, VGG19, TDVGGNetV1, TDVGG16, TDVGG19
-from .resnet import ResNetV1, ResNet18V1
