a30c2a5270aae56c15a67fbc8040908ede2ad466
Jesus Villalba <jesus.antonio.villalba@gmail.com>
2020-08-05 18:20:16 -0400

    updated voxceleb/v1.1 datapaths

--- egs/voxceleb/v1.1/datapath.sh
+++ egs/voxceleb/v1.1/datapath.sh
@@ -3,16 +3,11 @@
 #
 # Paths to the databases used in the experiment
 
-#master key
-master_key_dir=master_key_sre04-12
-master_key=$master_key_dir/NIST_SRE_segments_key.v2.csv
-
-#paths to databases
 
 if [ "$(hostname --domain)" == "clsp.jhu.edu" ];then
-    voxceleb1_root=/export/corpora/VoxCeleb1
-    voxceleb2_root=/export/corpora/VoxCeleb2
-    musan_root=/export/corpora/JHU/musan
+    voxceleb1_root=/export/corpora5/VoxCeleb1_v1
+    voxceleb2_root=/export/corpora5/VoxCeleb2
+    musan_root=/export/corpora5/JHU/musan
 elif [ "$(hostname --domain)" == "cm.gemini" ];then
     voxceleb1_root=/expscratch/dsnyder/VoxCeleb1
     voxceleb2_root=/expscratch/dgromero/corpora-open/vox2
