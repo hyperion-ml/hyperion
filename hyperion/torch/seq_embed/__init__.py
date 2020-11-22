479ef50ea999d29ba7ddd81fae9ef8bc2cdb5405
Magdalena Rybicka <magdalena.marta.rybicka@gmail.com>
2020-11-22 17:39:06 -0500

    add spine confs

--- hyperion/torch/seq_embed/__init__.py
+++ hyperion/torch/seq_embed/__init__.py
@@ -10,3 +10,4 @@ from .tdnn_xvector import TDNNXVector
 from .resnet_xvector import ResNetXVector
 from .efficient_net_xvector import EfficientNetXVector
 from .transformer_xvector_v1 import TransformerXVectorV1
+from .spinenet_xvector import SpineNetXVector
