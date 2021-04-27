722d030ccca4a39fa0213651648c016d7bea3e20
Jesus Villalba <jesus.antonio.villalba@gmail.com>
2021-04-26 20:51:46 -0400

    voxceleb vae v1 up to step 12

--- hyp_utils/generative/eval_vae.sh
+++ hyp_utils/generative/eval_vae.sh
@@ -71,10 +71,10 @@ if [ "$use_gpu" == "true" ];then
 fi
 
 if [ "$center" == "false" ];then
-    args="${args} --no-norm-mean"
+    args="${args} --mvn.no-norm-mean"
 fi
 if [ "$norm_var" == "true" ];then
-    args="${args} --norm-var"
+    args="${args} --mvn.norm-var"
 fi
 
 if [ "$use_bin_vad" == "true" ];then
@@ -116,7 +116,7 @@ if [ $stage -le 0 ];then
     $cmd JOB=1:$nj $output_dir/log/eval_vae.JOB.log \
 	hyp_utils/conda_env.sh --num-gpus $num_gpus \
 	torch-eval-vae.py ${args}  \
-	--left-context $left_context --right-context $right_context \
+	--mvn.left-context $left_context --mvn.right-context $right_context \
 	--part-idx JOB --num-parts $nj \
 	--input scp:$data_dir/feats.scp \
 	--model-path $nnet_file \
