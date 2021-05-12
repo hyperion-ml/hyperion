a391d121e3302f1905d8f18aaa8f845b376d84aa
Jesus Villalba <jesus.antonio.villalba@gmail.com>
2021-05-12 14:25:47 -0400

    chime5 up to step 2

--- egs/chime5_spkdet/v1/run_002_compute_evad.sh
+++ egs/chime5_spkdet/v1/run_002_compute_evad.sh
@@ -42,11 +42,7 @@ fi
 
 # Train/Test Datasets
 if [ $stage -le 2 ];then 
-    for name in voxcelebcat \
-    	sitw_dev_enroll sitw_dev_test sitw_eval_enroll sitw_eval_test \
-        sre18_dev_test_vast sre18_eval_test_vast \
-	sre19_av_a_dev_test sre19_av_a_eval_test \
-	janus_dev_enroll janus_dev_test_core janus_eval_enroll janus_eval_test_core
+    for name in chime5_spkdet_enroll chime5_spkdet_test voxcelebcat
     do
 	num_spk=$(wc -l data/$name/spk2utt | awk '{ print $1}')
 	nj=$(($num_spk < 40 ? $num_spk:40))
@@ -55,43 +51,6 @@ if [ $stage -le 2 ];then
 	    data/${name} exp/make_vad/$name $vaddir
 	utils/fix_data_dir.sh data/${name}
     done
-fi
-
-#Enroll multi-speaker Datasets with time marks
-if [ $stage -le 3 ];then 
-    for name in sre18_dev_enroll_vast sre18_eval_enroll_vast sre19_av_a_dev_enroll sre19_av_a_eval_enroll
-    do
-	num_spk=$(wc -l data/$name/spk2utt | awk '{ print $1}')
-	nj=$(($num_spk < 40 ? $num_spk:40))
-	# we just run energy vad to get the utt2num_frames file
-	hyp_utils/feats/make_evad.sh --write-utt2num-frames true \
-	    --vad-config $vad_config --nj $nj --cmd "$train_cmd" \
-	    data/${name} exp/make_vad/$name $vaddir
-	utils/fix_data_dir.sh data/${name}
-	local/sre18_diar_to_vad.sh data/${name} exp/make_vad $vaddir
-	utils/fix_data_dir.sh data/${name}
-    done
-fi
-
-#Dihard Datasets
-if [ $stage -le 4 ];then
-    for name in dihard2_train_dev dihard2_train_eval
-    do
-	num_spk=$(wc -l data/$name/spk2utt | awk '{ print $1}')
-	nj=$(($num_spk < 40 ? $num_spk:40))
-	# we just run energy vad to get the utt2num_frames file
-	hyp_utils/feats/make_evad.sh --write-utt2num-frames true \
-	    --vad-config $vad_config --nj $nj --cmd "$train_cmd" \
-	    data/${name} exp/make_vad/$name $vaddir
-	hyp_utils/rttm_to_bin_vad.sh --nj 5 data/$name/vad.rttm data/$name $vaddir
-	utils/fix_data_dir.sh data/${name}
-    done
-
-fi
-
-if [ $stage -le 5 ];then 
-  utils/combine_data.sh --extra-files "utt2num_frames" data/dihard2_train data/dihard2_train_dev data/dihard2_train_eval
-  utils/fix_data_dir.sh data/dihard2_train
 fi
 
 
