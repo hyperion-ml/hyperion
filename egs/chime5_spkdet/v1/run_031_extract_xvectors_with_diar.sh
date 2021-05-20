50f3c2cd378033a6a2ec3ec66a1d2301dd49a5a7
Jesus Villalba <jesus.antonio.villalba@gmail.com>
2021-05-20 15:56:40 -0400

    chime5 recipe works

--- egs/chime5_spkdet/v1/run_031_extract_xvectors_with_diar.sh
+++ egs/chime5_spkdet/v1/run_031_extract_xvectors_with_diar.sh
@@ -26,7 +26,7 @@ xvector_dir=exp/xvectors/$nnet_name
 
 if [ $stage -le 1 ]; then
     # Extracts x-vectors for evaluation
-    for name in chime5_spkdet_test chime5_spkdet_test_gtvad
+    for name in chime5_spkdet_test
     do
 	name_out=${name}_${diar_name}
 	num_spk=$(wc -l data/$name/spk2utt | awk '{ print $1}')
@@ -38,47 +38,3 @@ if [ $stage -le 1 ]; then
 	    $xvector_dir/$name_out
     done
 fi
-exit
-if [ $stage -le 2 ]; then
-    # combine datasets    
-    mkdir -p $xvector_dir/sitw_dev_${diar_name}
-    cat $xvector_dir/sitw_dev_{enroll,test_${diar_name}}/xvector.scp \
-	> $xvector_dir/sitw_dev_${diar_name}/xvector.scp
-
-    # mkdir -p $xvector_dir/sitw_eval_${diar_name}
-    # cat $xvector_dir/sitw_eval_{enroll,test_${diar_name}}/xvector.scp \
-    # 	> $xvector_dir/sitw_eval_${diar_name}/xvector.scp
-    
-    mkdir -p $xvector_dir/sre18_dev_vast_${diar_name}
-    cat $xvector_dir/sre18_dev_{enroll_vast,test_vast_${diar_name}}/xvector.scp \
-	> $xvector_dir/sre18_dev_vast_${diar_name}/xvector.scp
-
-    # mkdir -p $xvector_dir/sre18_eval_vast_${diar_name}
-    # cat $xvector_dir/sre18_eval_{enroll_vast,test_vast_${diar_name}}/xvector.scp \
-    # 	> $xvector_dir/sre18_eval_vast_${diar_name}/xvector.scp
-
-    # mkdir -p $xvector_dir/sre19_av_a_dev_${diar_name}
-    # cat $xvector_dir/sre19_av_a_dev_{enroll,test_${diar_name}}/xvector.scp \
-    # 	> $xvector_dir/sre19_av_a_dev_${diar_name}/xvector.scp
-
-    # mkdir -p $xvector_dir/sre19_av_a_eval_${diar_name}
-    # cat $xvector_dir/sre19_av_a_eval_{enroll,test_${diar_name}}/xvector.scp \
-    # 	> $xvector_dir/sre19_av_a_eval_${diar_name}/xvector.scp
-
-    # mkdir -p $xvector_dir/janus_dev_core_${diar_name}
-    # cat $xvector_dir/janus_dev_{enroll,test_core_${diar_name}}/xvector.scp > \
-    # 	$xvector_dir/janus_dev_core_${diar_name}/xvector.scp
-
-    # mkdir -p $xvector_dir/janus_eval_core_${diar_name}
-    # cat $xvector_dir/janus_eval_{enroll,test_core_${diar_name}}/xvector.scp \
-    # 	> $xvector_dir/janus_eval_core_${diar_name}/xvector.scp
-
-fi
-
-if [ $stage -le 3 ]; then
-    mkdir -p $xvector_dir/sitw_sre18_dev_vast_${diar_name}
-    cat $xvector_dir/{sitw_dev_${diar_name},sre18_dev_vast_${diar_name}}/xvector.scp \
-	> $xvector_dir/sitw_sre18_dev_vast_${diar_name}/xvector.scp
-fi
-
-exit
