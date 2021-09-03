2b6d876c3579116d70d92562e5ce6ff00a9408af
Jesus Villalba <jesus.antonio.villalba@gmail.com>
2021-09-03 16:30:21 -0400

    sre21 8k up to step 3

--- egs/sre21-av-a/v1.8k/run_003_prepare_noises_rirs.sh
+++ egs/sre21-av-a/v1.8k/run_003_prepare_noises_rirs.sh
@@ -18,12 +18,12 @@ if [ $stage -le 1 ]; then
 
     # Prepare the MUSAN corpus, which consists of music, speech, and noise
     # suitable for augmentation.
-    local/make_musan.sh $musan_root 16 data
+    local/make_musan.sh $musan_root 8 data
     
     for name in musan_noise musan_music
     do
 	steps_xvec/preprocess_audios_for_nnet_train.sh --nj 10 --cmd "$train_cmd" \
-	    --storage_name sre21-v1.16k-$(date +'%m_%d_%H_%M') \
+	    --storage_name sre21-v1.8k-$(date +'%m_%d_%H_%M') \
 	    data/${name} data/${name}_proc_audio exp/${name}_proc_audio
 	utils/fix_data_dir.sh data/${name}_proc_audio
     done
@@ -36,7 +36,7 @@ if [ $stage -le 2 ]; then
     for name in musan_speech
     do
 	steps_xvec/make_babble_noise_for_nnet_train.sh --cmd "$train_cmd" \
-	    --storage_name sre21-v1.16k-$(date +'%m_%d_%H_%M') \
+	    --storage_name sre21-v1.8k-$(date +'%m_%d_%H_%M') \
 	    data/${name} data/${name}_babble exp/${name}_babble
 	# utils/fix_data_dir.sh data/${name}_babble
     done
@@ -44,17 +44,17 @@ fi
 
 if [ $stage -le 3 ]; then
     if [ ! -d "RIRS_NOISES" ]; then
-	if [ -d ../../sre19-cmn2/v2.1/RIRS_NOISES ];then
-	    ln -s ../../sre19-cmn2/v2.1/RIRS_NOISES
+	if [ -d ../v1.16k/RIRS_NOISES ];then
+	    ln -s ../v1.16k/RIRS_NOISES
 	else
 	    # Download the package that includes the real RIRs, simulated RIRs, isotropic noises and point-source noises
 	    wget --no-check-certificate http://www.openslr.org/resources/28/rirs_noises.zip
 	    unzip rirs_noises.zip
 	fi
     fi
-    local/make_rirs_data.sh RIRS_NOISES/simulated_rirs/smallroom 16 data/rirs_smallroom
-    local/make_rirs_data.sh RIRS_NOISES/simulated_rirs/mediumroom 16 data/rirs_mediumroom
-    local/make_rirs_data.sh RIRS_NOISES/real_rirs_isotropic_noises 16 data/rirs_real
+    local/make_rirs_data.sh RIRS_NOISES/simulated_rirs/smallroom 8 data/rirs_smallroom
+    local/make_rirs_data.sh RIRS_NOISES/simulated_rirs/mediumroom 8 data/rirs_mediumroom
+    local/make_rirs_data.sh RIRS_NOISES/real_rirs_isotropic_noises 8 data/rirs_real
     for rirs in rirs_smallroom rirs_mediumroom rirs_real
     do
 	#pack all rirs in h5 files
