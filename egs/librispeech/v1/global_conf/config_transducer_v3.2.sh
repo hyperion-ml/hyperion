43f6c4cdaa035c6a626f46c599c183c9cb5efc2f
Jesus Villalba <jesus.antonio.villalba@gmail.com>
2022-12-15 10:15:23 -0500

    more transducer configs

---
+++ egs/librispeech/v1/global_conf/config_transducer_v3.2.sh
@@ -0,0 +1,39 @@
+# WavLM base trained on 60k LibriLight + 10k GigaSpeech + 24k Voxpopuli + ECAPA-TDNN 512x3
+
+# hugging face model
+hf_model_name=wav2vec2xlsr300m
+
+#vad
+# vad_config=conf/vad_16k.yaml
+
+# x-vector training 
+nnet_data=train_clean_100
+dev_data=dev_clean
+# nnet_data=train_clean_small
+
+bpe_model=data/lang_bpe_1000/bpe.model
+# x-vector cfg
+
+nnet_type=hf_wav2vec2transducer
+
+nnet_s1_base_cfg=conf/train_wav2vec2xlsr300m_transducer_stage1_v3.2.yaml
+nnet_s1_args=""
+
+nnet_name=${hf_model_name}_transducer_v3.2
+nnet_s1_name=$nnet_name.s1
+
+nnet_s1_dir=exp/transducer_nnets/$nnet_s1_name
+nnet_s1=$nnet_s1_dir/model_ep0060.pth
+
+nnet_s2_base_cfg=conf/train_wav2vec2xlsr300m_transducer_stage1_v1.0.yaml
+nnet_s2_args=""
+nnet_s2_name=${nnet_name}.s2
+nnet_s2_dir=exp/transducer_nnets/$nnet_s2_name
+nnet_s2=$nnet_s2_dir/model_ep0020.pth
+
+nnet_s3_base_cfg=conf/train_wav2vec2xlsr300m_transducer_stage1_v1.0.yaml
+nnet_s3_args=""
+nnet_s3_name=${nnet_name}.s3
+nnet_s3_dir=exp/transducer_nnets/$nnet_s3_name
+nnet_s3=$nnet_s3_dir/model_ep0002.pth
+nnet_s3=$nnet_s3_dir/model_ep0005.pth
