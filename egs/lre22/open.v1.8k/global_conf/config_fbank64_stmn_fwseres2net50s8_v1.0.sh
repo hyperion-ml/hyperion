e49157dfa1420091e603bf832abfe4e5acf1d158
System User <lgarcia@test1.cm.gemini>
2023-11-30 13:11:57 -0500

    clean up recipe lre22/open.v2.8k

--- egs/lre22/open.v1.8k/global_conf/config_fbank64_stmn_fwseres2net50s8_v1.0.sh
+++ egs/lre22/open.v1.8k/global_conf/config_fbank64_stmn_fwseres2net50s8_v1.0.sh
@@ -13,7 +13,7 @@ nnet_type=resnet
 nnet_stages=1
 nnet_s1_base_cfg=conf/train_fwseres2net50s8_xvec_stage1_v1.0.yaml
 
-nnet_name=${feat_type}_fwseres2net50s8_v1.2
+nnet_name=${feat_type}_fwseres2net50s8_v1.0
 nnet_s1_name=$nnet_name.s1
 nnet_s1_dir=exp/xvector_nnets/$nnet_s1_name
 nnet_s1=$nnet_s1_dir/model_ep0012.pth
