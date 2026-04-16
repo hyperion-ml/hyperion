48c4d0a364e082c508bd0241677eec8d1b07b9d6
Jesus Villalba <jesus.antonio.villalba@gmail.com>
2026-04-16 15:25:00 -0400

    add typehints to torch/utils/distributions

--- hyperion/torch/utils/distributions.py
+++ hyperion/torch/utils/distributions.py
@@ -1,29 +1,38 @@
 """
- Copyright 2020 Johns Hopkins University  (Author: Jesus Villalba)
- Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+Copyright 2020 Johns Hopkins University  (Author: Jesus Villalba)
+Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
 """
+
 import re
+from typing import Dict, Mapping
 
 import torch
 import torch.distributions as dists
 
+PdfDict = Dict[str, torch.Tensor]
 
-def squeeze_pdf(pdf, dim):
 
+def squeeze_pdf(pdf: dists.Distribution, dim: int) -> dists.Distribution:
+    """Returns a copy of ``pdf`` with ``dim`` squeezed from distribution tensors."""
     if isinstance(pdf, dists.normal.Normal):
         loc = pdf.loc.squeeze(dim=dim)
         scale = pdf.scale.squeeze(dim=dim)
         return dists.normal.Normal(loc=loc, scale=scale)
 
+    raise NotImplementedError(f"Unsupported distribution type: {type(pdf)}")
 
-def squeeze_pdf_(pdf, dim):
 
+def squeeze_pdf_(pdf: dists.Distribution, dim: int) -> None:
+    """Squeezes ``dim`` in-place from distribution tensors."""
     if isinstance(pdf, dists.normal.Normal):
         pdf.loc.squeeze_(dim=dim)
         pdf.scale.squeeze_(dim=dim)
+        return
+
+    raise NotImplementedError(f"Unsupported distribution type: {type(pdf)}")
 
 
-def serialize_pdf_to_dict(pdf):
+def serialize_pdf_to_dict(pdf: dists.Distribution) -> PdfDict:
     """Serializes pdfs to a dictionary
 
     When we want to return a pdf in a forward function,
@@ -37,8 +46,8 @@ def serialize_pdf_to_dict(pdf):
         raise NotImplementedError()
 
 
-def deserialize_pdf_from_dict(pdf):
-    """Derializes pdfs from a dictionary
+def deserialize_pdf_from_dict(pdf: Mapping[str, torch.Tensor]) -> dists.Distribution:
+    """Deserializes pdfs from a dictionary.
 
     When we want to return a pdf in a forward function,
     and we are using DataParallel, we need to transform the pdf into a
@@ -47,8 +56,13 @@ def deserialize_pdf_from_dict(pdf):
 
     This function will transform the dictionary back into torch.distribution objects
     """
-    pdf_type = re.sub(r".*", "", pdf.keys()[0])
+    if len(pdf) == 0:
+        raise ValueError("pdf dictionary is empty")
+
+    pdf_type = re.sub(r"\..*$", "", next(iter(pdf)))
     if pdf_type == "normal":
+        if "normal.loc" not in pdf or "normal.scale" not in pdf:
+            raise KeyError("normal pdf requires keys: 'normal.loc' and 'normal.scale'")
         return dists.normal.Normal(loc=pdf["normal.loc"], scale=pdf["normal.scale"])
     else:
         raise NotImplementedError()
