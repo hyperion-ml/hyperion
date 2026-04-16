6d2b4aa599bfa64943ff3fc67ea34339ef2c1e8b
Jesus Villalba <jesus.antonio.villalba@gmail.com>
2026-04-16 10:53:39 -0400

    review docs in collation

--- hyperion/torch/utils/collation.py
+++ hyperion/torch/utils/collation.py
@@ -1,40 +1,44 @@
 """
- Copyright 2022 Johns Hopkins University  (Author: Jesus Villalba, Nanxin Chen)
- Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+Copyright 2022 Johns Hopkins University  (Author: Jesus Villalba, Nanxin Chen)
+Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
 """
 
+from typing import Any, Dict, List, Tuple, Union
+
 import torch
-import torch.nn as nn
 from torch.nn.utils.rnn import pad_sequence
 
+Batch = Union[List[Any], Tuple[Any, ...]]
+ListOfDicts = Union[List[Dict[str, Any]], Tuple[Dict[str, Any], ...]]
 
-def list_of_dicts_to_list(list_of_dicts, key):
-    """Takes a list of dictionaries and a key,
-    and returns a list of the items corresponding to the key
-    """
-    output = []
+
+def list_of_dicts_to_list(list_of_dicts: ListOfDicts, key: str) -> List[Any]:
+    """Returns values for ``key`` from each dict in ``list_of_dicts``."""
+    output: List[Any] = []
     for item in list_of_dicts:
         output.append(item[key])
 
     return output
 
 
-def collate_seqs_1d(x, pad_value=0):
-    """Combines a list/tuple of vectors with different lengths
-       into a single tensor.
+def collate_seqs_1d(
+    x: Batch, pad_value: float = 0.0
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    """Pads a batch of 1-D sequences to the same length.
 
     Args:
-        x: input lits/tuple of vectors.
+        x: List/tuple of 1-D tensors (or numpy arrays) with variable lengths.
+        pad_value: Padding value used by ``torch.nn.utils.rnn.pad_sequence``.
 
     Returns:
-      2D tensor with shape (num_vectors, max_vector_length).
-      1D long tensor containing the vector lengths.
+        Tensor of shape ``(batch, max_length)`` containing padded sequences.
+        Tensor of shape ``(batch,)`` with the original sequence lengths.
     """
     if not isinstance(x[0], torch.Tensor):
         x = [torch.from_numpy(x_i) for x_i in x]
 
     assert x[0].dim() == 1
-    x_lengths = []
+    x_lengths: List[int] = []
     for x_i in x:
         x_lengths.append(x_i.size(0))
 
@@ -43,18 +47,20 @@ def collate_seqs_1d(x, pad_value=0):
     return x, x_lengths
 
 
-def collate_seqs_2d(x, pad_value=0, pad_dim=0):
-    """Combines a list/tuple of matrices with different sizes in one of
-       the dimensions into a single 3d tensor.
-       Combines performing padding on the dimension which is not constant.
+def collate_seqs_2d(
+    x: Batch, pad_value: float = 0.0, pad_dim: int = 0
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    """Pads a batch of 2-D tensors along a selected dimension.
 
     Args:
-        x: input lits/tuple of matrices.
-        pad_dim: padding dimension.
+        x: List/tuple of 2-D tensors (or numpy arrays).
+        pad_value: Padding value used by ``torch.nn.utils.rnn.pad_sequence``.
+        pad_dim: Dimension to pad. Supports negative values.
 
     Returns:
-      3D tensor with shape (num_vectors, max_length, feat_dim) or (num_vectors, feat_dim, length).
-      1D long tensor containing the dimensions lengths.
+        Tensor with shape ``(batch, max_length, feat_dim)`` when ``pad_dim=0``
+        or ``(batch, feat_dim, max_length)`` when ``pad_dim=1``.
+        Tensor of shape ``(batch,)`` with lengths along the padded dimension.
     """
     if not isinstance(x[0], torch.Tensor):
         x = [torch.from_numpy(x_i) for x_i in x]
@@ -65,7 +71,7 @@ def collate_seqs_2d(x, pad_value=0, pad_dim=0):
     if pad_dim != 0:
         x = [x_i.transpose(pad_dim, 0) for x_i in x]
 
-    x_lengths = []
+    x_lengths: List[int] = []
     for x_i in x:
         x_lengths.append(x_i.size(0))
 
@@ -77,18 +83,19 @@ def collate_seqs_2d(x, pad_value=0, pad_dim=0):
     return x, x_lengths
 
 
-def collate_seqs_nd(x, pad_value=0, pad_dim=0):
-    """Combines a list/tuple of N-d tensors with different sizes in one of
-       the dimensions into a single (N+1)-d tensor.
-       Combines performing padding on the dimension which is not constant.
+def collate_seqs_nd(
+    x: Batch, pad_value: float = 0.0, pad_dim: int = 0
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    """Pads a batch of N-D tensors along a selected dimension.
 
     Args:
-        x: input lits/tuple of matrices.
-        pad_dim: padding dimension.
+        x: List/tuple of N-D tensors (or numpy arrays).
+        pad_value: Padding value used by ``torch.nn.utils.rnn.pad_sequence``.
+        pad_dim: Dimension to pad. Supports negative values.
 
     Returns:
-      (N+1)-d combined tensor.
-      1D long tensor containing the dimensions lengths.
+        Padded tensor with one extra batch dimension at axis 0.
+        Tensor of shape ``(batch,)`` with lengths along the padded dimension.
     """
     if not isinstance(x[0], torch.Tensor):
         x = [torch.from_numpy(x_i) for x_i in x]
@@ -102,7 +109,7 @@ def collate_seqs_nd(x, pad_value=0, pad_dim=0):
     if pad_dim != 0:
         x = [x_i.transpose(pad_dim, 0) for x_i in x]
 
-    x_lengths = []
+    x_lengths: List[int] = []
     for x_i in x:
         x_lengths.append(x_i.size(0))
 
@@ -112,88 +119,3 @@ def collate_seqs_nd(x, pad_value=0, pad_dim=0):
         x = x.transpose(1, pad_dim + 1)
 
     return x, x_lengths
-
-
-# def collate_seq_1d(x, pad_value=0):
-#     """Combines a list/tuple of vectors with different lengths
-#        into a single tensor.
-
-#     Args:
-#         x: input lits/tuple of vectors.
-
-#     Returns:
-#       2D tensor with shape (num_vectors, max_vector_length).
-#       1D long tensor containing the vector lengths.
-#     """
-#     max_length = max([x_i.size(0) for x_i in x])
-#     y = pad_value * torch.ones(len(x), max_length, dtype=x[0].dtype, device=x[0].device)
-#     y_lengths = torch.empty(len(x), dtype=torch.long, device=x[0].device)
-#     for i, x_i in enumerate(x):
-#         y[i, : x_i.size(0)] = x_i
-#         y_lengths[i] = x_i.size(0)
-
-#     return y, y_lengths
-
-
-# def collate_seq_2d(x, pad_value=0, pad_dim=-1):
-#     """Combines a list/tuple of matrices with different sizes in one of
-#        the dimensions into a single 3d tensor.
-#        Combines performing padding on the dimension which is not constant.
-
-#     Args:
-#         x: input lits/tuple of matrices.
-#         pad_dim: padding dimension.
-
-#     Returns:
-#       3D tensor with shape (num_vectors, max_length, feat_dim) or (num_vectors, feat_dim, length).
-#       1D long tensor containing the dimensions lengths.
-#     """
-#     max_length = max([x_i.size(pad_dim) for x_i in x])
-#     y_size = list(x[0].size())
-#     y_size[pad_dim] = max_length
-#     y = pad_value * torch.ones(*y_size, dtype=x[0].dtype, device=x[0].device)
-#     y_lengths = torch.empty(len(x), dtype=torch.long, device=x[0].device)
-#     if pad_dim == -1 or pad_dim == 1:
-#         for i, x_i in enumerate(x):
-#             y[i, :, : x_i.size(pad_dim)] = x_i
-#             y_lengths[i] = x_i.size(pad_dim)
-#     else:
-#         for i, x_i in enumerate(x):
-#             y[i, : x_i.size(pad_dim)] = x_i
-#             y_lengths[i] = x_i.size(pad_dim)
-
-#     return y, y_lengths
-
-
-# def collate_seq_nd(x, pad_value=0, pad_dim=-1):
-#     """Combines a list/tuple of N-d tensors with different sizes in one of
-#        the dimensions into a single (N+1)-d tensor.
-#        Combines performing padding on the dimension which is not constant.
-
-#     Args:
-#         x: input lits/tuple of matrices.
-#         pad_dim: padding dimension.
-
-#     Returns:
-#       (N+1)-D combined tensor.
-#       1D long tensor containing the dimensions lengths.
-#     """
-#     if x[0].dim() == 1:
-#         return collate_seq_1d(x)
-
-#     if x[0].dim() == 2:
-#         return collate_seq_2d(x)
-
-#     # here the general case
-#     max_length = max([x_i.size(pad_dim) for x_i in x])
-#     y_trans_size = list(x[0].transpose(0, pad_dim).size())
-#     y = pad_value * torch.ones(*y_trans_size, dtype=x[0].dtype, device=x[0].device)
-#     y_lengths = torch.empty(len(x), dtype=torch.long, device=x[0].device)
-#     for i, x_i in enumerate(x):
-#         y[i, : x_i.size(pad_dim)] = x_i.transpose(0, pad_dim)
-#         y_lengths[i] = x_i.size(pad_dim)
-
-#     if pad_dim > 0:
-#         pad_dim = pad_dim + 1
-#     y = y.transpose(1, pad_dim).contiguous()
-#     return y, y_lengths
